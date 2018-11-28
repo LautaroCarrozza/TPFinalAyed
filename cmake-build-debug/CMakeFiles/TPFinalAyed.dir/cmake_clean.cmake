94791ec62814e635f8e5c0620d26bde10ee25ea4
Lautaro Carrozza <lautaro.carrozza@ing.austral.edu.ar>
2018-11-28 19:15:19 -0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/TPFinalAyed.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPFinalAyed.dir/main.c.obj"
+  "TPFinalAyed.pdb"
+  "TPFinalAyed.exe"
+  "TPFinalAyed.exe.manifest"
+  "libTPFinalAyed.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TPFinalAyed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
